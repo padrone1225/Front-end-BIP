82a2887aee9ea0ec90f34d32c04f2c25aa16ff5f
padrone1225 <christoper.davis.25@gmail.com>
2023-05-21 04:47:05 +0900

    basic structure as React, TypeScript, Vite, TailwindCSS and Jest

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+const testIds = {};
+
+const renderApp = () => render(<App />);
+
+test("Initial render correctly", () => {
+  const { queryAllByTestId } = renderApp();
+  const textelement = queryAllByTestId("app-link");
+  console.log("TEST--------->", textelement[0].textContent, typeof textelement);
+});
