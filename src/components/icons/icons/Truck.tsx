7efc5decde8586e8823e5bcfe49b9b2406e64730
padrone1225 <christoper.davis.25@gmail.com>
2023-05-21 07:31:13 +0900

    add icon svg v1

---
+++ src/components/icons/icons/Truck.tsx
@@ -0,0 +1,47 @@
+export default function Truck() {
+  return (
+    <svg
+      width="22"
+      height="17"
+      viewBox="0 0 22 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 13H13V1H1V13H4"
+        stroke="#030912"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M19 13H21V9.66C21.0004 9.13456 20.8973 8.61418 20.6965 8.1286C20.4958 7.64302 20.2013 7.20175 19.83 6.83L18 5H13"
+        stroke="#030912"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13 13H14"
+        stroke="#030912"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.5 16C7.88071 16 9 14.8807 9 13.5C9 12.1193 7.88071 11 6.5 11C5.11929 11 4 12.1193 4 13.5C4 14.8807 5.11929 16 6.5 16Z"
+        stroke="#030912"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.5 16C17.8807 16 19 14.8807 19 13.5C19 12.1193 17.8807 11 16.5 11C15.1193 11 14 12.1193 14 13.5C14 14.8807 15.1193 16 16.5 16Z"
+        stroke="#030912"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
